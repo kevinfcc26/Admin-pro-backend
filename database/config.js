6dce95a37151cdbe137529f0251f9b4b2c3f23a6
kevinfcc26 <kevinfcc26@gmail.com>
2020-10-20 16:57:11 -0500

    CORS y Express

---
+++ database/config.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const dbConnection = async() => {
+
+    try {
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('DB Online');
+    } catch (error){
+        console.log(error);
+        throw new Error('Error a la hora de tirar la db ver logs');
+
+    }
+};
+
+module.exports = {
+    dbConnection
+};
