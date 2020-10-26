c7cad0d58d96c29bd80557b97e74263d796d5a93
kevinfcc26 <kevinfcc26@gmail.com>
2020-10-26 16:55:23 -0500

    End section 10 - Auth

--- index.js
+++ index.js
@@ -11,19 +11,18 @@ const app = express();
 // Configurar cors
 app.use(cors());
 
+// Lectura y parceo del body
+app.use(express.json());
+
 // Base de datos
 dbConnection();
 
 
 // Rutas
-app.get('/', (req, res) => {
 
-    res.json({
-        ok: true,
-        msg: 'Hola mundo'
-    });
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
-});
 
 app.listen( process.env.PORT, () => {
     console.log('servidor corriendo en el puerto ' + process.env.PORT);
